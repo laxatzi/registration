c82f96a06e6586336ac0101246605721a4f7aa26
laxatzi <laxatzi@yahoo.com>
2025-05-05 16:49:01 +0300

    Hash password

--- App/Models/User.php
+++ App/Models/User.php
@@ -31,6 +31,18 @@ class User extends \Core\Model {
      */
     public function save() {
 
+    $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+
+     $sql = 'INSERT INTO USERS (name, email, password_hash ) VALUES (:name, :email, :password_hash)';
+
+     $db = static::getDB();
+
+     $stmt = $db->prepare($sql);
+     $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
+     $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+     $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+
+     $stmt->execute();
 
     }
 }
