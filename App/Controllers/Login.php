05085a75c3cca6f06eb5617653e85259e2dac40c
laxatzi <laxatzi@yahoo.com>
2025-05-19 12:17:32 +0300

    INIT: login controller

---
+++ App/Controllers/Login.php
@@ -0,0 +1,24 @@
+<?php
+
+  namespace App\Controllers;
+
+  use \Core\View;
+
+  /**
+   * Login Controller
+   *
+   *
+   */
+
+   Class Login extends \Core\Controller {
+    /**
+     * Show the login page
+     *
+     * return void
+     *
+     */
+
+     public function newAction() {
+
+     }
+   }
