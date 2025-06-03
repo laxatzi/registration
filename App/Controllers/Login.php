3bcabd2269d55744bae460937217985071bd8b39
laxatzi <laxatzi@yahoo.com>
2025-06-03 15:36:19 +0300

    Call redirect function in Login and Signup controllers

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -35,8 +35,7 @@
       $user = User::authenticate($_POST['email'], $_POST['password']);
       /** If $user authenticates we direct the page to the home page Otherwise will display the login page again */
       if ($user) {
-        header('Location: http://'. $_SERVER['HTTP_HOST']. '/', TRUE, 303);
-        exit;
+        $this->redirect('/');
       }
       else {
         View::renderTemplate('Login/login.html', [
