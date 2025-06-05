8c841a337f66d92dca8fdec9b459e34f09cfd9e0
laxatzi <laxatzi@yahoo.com>
2025-06-05 12:02:34 +0300

    Create logout method in Auth class

--- App/Auth.php
+++ App/Auth.php
@@ -26,4 +26,30 @@ class Auth
         $_SESSION['user_id'] = $user->id;
   }
 
+  /**
+   * Logout the user by clearing the session.
+   * @return void
+   *
+   */
+
+public static function logout()   {
+   /// Unset all of the session variables.
+        $_SESSION = array();
+
+        // If it's desired to kill the session, also delete the session cookie.
+        // Note: This will destroy the session, and not just the session data!
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+
+        // Finally, destroy the session.
+        session_destroy();
+        // redirect to the login page
+
+}
+
 }
