e49460ce443da3635f87ac794bfe8a351a4be5d7
laxatzi <laxatzi@yahoo.com>
2025-06-18 12:41:57 +0300

    Remove before filter from this controller

--- App/Controllers/Restrictor.php
+++ App/Controllers/Restrictor.php
@@ -11,16 +11,6 @@
 
   class Restrictor extends \Core\Controller {
 
-  /**
-     * Require the user to be authenticated before allowing access to the action methods
-     *
-     * @return void
-     */
-    protected function before() {
-
-        $this->redirect('/login');
-      }
-
 
 
     /**
