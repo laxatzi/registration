2caca2ac8a99123d47234aeed29b489af2ae592d
laxatzi <laxatzi@yahoo.com>
2025-06-20 16:34:44 +0300

    Init Token class

---
+++ App/Token.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+/*
+  * Token
+  *
+  * PHP version 7.0
+  */
+
+class Token {
+  /*
+    * the token value
+    *
+    * @var array
+    */
+
+    protected $token;
+    /**
+     * Constructor
+     *
+     * @return void
+     */
+
+
+}
