b3a38482f959bb82d0b22143311f934c85ae0a41
laxatzi <laxatzi@yahoo.com>
2025-06-20 17:05:34 +0300

    contstruct and retrieve hash value

--- App/Token.php
+++ App/Token.php
@@ -23,4 +23,31 @@ class Token {
      */
 
 
+    public function __construct() {
+        $this->token = bin2hex(random_bytes(16));
+
+
+}
+
+    /*
+      * Get Token value
+      *
+      * @return string token value
+    */
+    public function getValue() {
+        return $this->token;
+    }
+
+    /*
+      * Get the Hashed Token value
+      *
+      * @return string hashed token value
+    */
+
+    public function getHashedValue() {
+        return password_hmac('sha256', $this->token, "PogN1yqiTIZaLNRSxlIkL0tscOnxsoFm");
+    }
+
+
+
 }
