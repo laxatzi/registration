945f3da9a7036cb52a9a238c86077814f7029579
laxatzi <laxatzi@yahoo.com>
2025-06-20 16:02:33 +0300

    alter PDO arguments

--- Core/Model.php
+++ Core/Model.php
@@ -24,7 +24,8 @@ abstract class Model
 
         if ($db === null) {
             $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' . Config::DB_NAME . ';charset=utf8';
-            $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
+            // $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
+            $db = new PDO('mysql:host=localhost;dbname=mvclogin;charset=utf8', 'root', '');
 
             // Throw an Exception when an error occurs
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
